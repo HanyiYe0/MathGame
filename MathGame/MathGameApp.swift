e36df44d0265dd7543599e0b90ddfb46dc08c500
HanyiYe0 <hanyiye2@gmail.com>
2025-04-01 13:48:09 -0400

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Hanyi Ye on 2025-04-01.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
