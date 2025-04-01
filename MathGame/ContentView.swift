2a66e95e20dd1a1f40c82e741c821820e16de35b
HanyiYe0 <hanyiye2@gmail.com>
2025-04-01 14:30:54 -0400

    Simple rectangle + selectable implemented

--- MathGame/ContentView.swift
+++ MathGame/ContentView.swift
@@ -7,18 +7,32 @@
 
 import SwiftUI
 
-struct ContentView: View {
+var nums: [[Int]] = [[1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8]]
+
+import SwiftUI
+
+
+struct ClickableSquareView: View {
+    @State private var borderWidth: Int = 0
+    @State private var clicked: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Rectangle()
+            .fill(Color.blue)
+            .frame(width: 100, height: 100)
+            .border(Color.black, width: CGFloat(borderWidth))
+            .onTapGesture {
+                borderWidth = 5
+                if clicked {
+                    clicked = false
+                    borderWidth = 0
+                } else {
+                    clicked = true
+                    borderWidth = 5
+                }
+            }
     }
 }
 
 #Preview {
-    ContentView()
+    ClickableSquareView()
 }
